3d2c35d01f9d12490fcac46f135ae89a84dd7639
zanspiler <zanspiler1@gmail.com>
2021-10-25 20:40:51 +0200

    Updated welcome page

--- client/src/components/layout/NavigationBar.js
+++ client/src/components/layout/NavigationBar.js
@@ -40,20 +40,6 @@ function NavigationBar() {
 					</span>
 				</div>
 			</Link>
-			{/* <div className="block lg:hidden">
-				<button className="flex items-center px-3 py-2 border rounded text-purple-200 border-purple-400 hover:text-white hover:border-white">
-					<svg
-						className="fill-current h-3 w-3"
-						viewBox="0 0 20 20"
-						xmlns="http://www.w3.org/2000/svg"
-					>
-						<title>Menu</title>
-						<path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
-					</svg>
-				</button>
-			</div> */}
-			{/* <div className="w-full block flex-grow lg:flex lg:items-center lg:w-auto"> */}
-			{/* <div className="lg:flex-grow"> */}
 			<div className="w-full block flex-grow sm:flex sm:items-center sm:w-auto">
 				<div className="sm:flex-grow">
 					{auth.token && (
--- client/src/components/pages/Welcome.js
+++ client/src/components/pages/Welcome.js
@@ -1,14 +1,38 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function Welcome() {
 	return (
-		<div>
-			* screenshot of chat etc.. *
-			<img
-				className="h-full"
-				src="https://mocah.org/thumbs/531080-cat-4k-large.jpg"
-				alt=""
-			/>
+		<div className="pt-24 mt-14">
+			<div className="container px-3 mx-auto flex flex-wrap flex-col md:flex-row items-center">
+				<div className="flex flex-col w-full md:w-2/5 justify-center items-start text-center md:text-left">
+					<p className="uppercase tracking-loose w-full">
+						Want to talk?
+					</p>
+					<h1 className="my-4 text-5xl font-bold leading-tight">
+						Chat
+					</h1>
+					<p className="leading-normal text-2xl mb-8">
+						Used by SpongeBob, Patrick etc!
+					</p>
+
+					<div>
+						<Link to="/signup">
+							<button className="justify-center mx-auto bg-purple-500 border mb-4 md:ml-1 rounded-full text-white py-4 px-8 border-white shadow focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out">
+								Sign Up
+							</button>
+						</Link>
+					</div>
+				</div>
+
+				<div className="w-full md:w-3/5 py-6 text-center">
+					<img
+						className="w-full md:w-5/5 z-50 opacity-90"
+						alt="chat-screetshot"
+						src="screenshot.png"
+					/>
+				</div>
+			</div>
 		</div>
 	);
 }
